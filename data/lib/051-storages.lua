c47d642dd433ee203a24928d86a64240fbea75ce
mattyx14 <mattyx14@users.noreply.github.com>
2015-04-19 10:00:38 -0500

    storages

---
+++ data/lib/051-storages.lua
@@ -0,0 +1,13 @@
+-- STORAGES --
+-- (HIGHLY RECOMMENDED -> ONE mission per STORAGE) --
+-- Sort it in Values -
+
+Storage = {
+	-- Promotion Storage cannot be changed, it is set in source code
+	QuestChests = {
+		-- Todo: Move to a seperate range
+		-- DemonHelmetQuestDemonShield = 2212,
+		-- DemonHelmetQuestDemonHelmet = 2213,
+		-- DemonHelmetQuestSteelBoots = 2214,
+	}
+}
