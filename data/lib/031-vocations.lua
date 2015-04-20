0e5aaa2acf1b041fedc015a401b4f3415943c372
mattyx14 <mattyx14@users.noreply.github.com>
2015-04-20 10:02:28 -0500

    update libs and xml's

--- data/lib/031-vocations.lua
+++ data/lib/031-vocations.lua
@@ -1,19 +1,19 @@
+function isRookie(cid)
+	return isInArray({0}, getPlayerVocation(cid))
+end
+
 function isSorcerer(cid)
-	return isInArray({1, 5}, getPlayerVocation(cid))
+	return isInArray({1, 2, 3, 4, 5}, getPlayerVocation(cid))
 end
 
 function isDruid(cid)
-	return isInArray({2, 6}, getPlayerVocation(cid))
+	return isInArray({6, 7, 8, 9, 10}, getPlayerVocation(cid))
 end
 
 function isPaladin(cid)
-	return isInArray({3, 7}, getPlayerVocation(cid))
+	return isInArray({11, 12, 13, 14, 15}, getPlayerVocation(cid))
 end
 
 function isKnight(cid)
-	return isInArray({4, 8}, getPlayerVocation(cid))
-end
-
-function isRookie(cid)
-	return isInArray({0}, getPlayerVocation(cid))
+	return isInArray({16, 17, 18, 19, 20}, getPlayerVocation(cid))
 end
