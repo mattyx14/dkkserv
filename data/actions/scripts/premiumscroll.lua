e3c3bbec7ad6055c524451d5a34d0b573f6fb617
Matt Gomez <mattyx14@users.noreply.github.com>
2019-12-29 13:59:17 -0600

    5 days

--- data/actions/scripts/premiumscroll.lua
+++ data/actions/scripts/premiumscroll.lua
@@ -1,6 +1,6 @@
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	player:addPremiumDays(6)
-	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, 'You gain 6 premium days time.')
+	player:addPremiumDays(5)
+	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, 'You gain 5 premium days time.')
 	player:save()
 	item:remove(1)
 	return true
