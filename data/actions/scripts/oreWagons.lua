6df764f149036d8cc6df8e0af051170183f3f9b5
Matt Gomez <mattyx14@users.noreply.github.com>
2020-01-03 13:50:55 -0600

    fix old varkhal wagon

--- data/actions/scripts/oreWagons.lua
+++ data/actions/scripts/oreWagons.lua
@@ -1,6 +1,6 @@
 local config = {
 	[50140] = Position(1545, 672, 12), -- Temple
-	[50141] = Position(1503, 704, 8), -- Enter City
+	[50141] = Position(1503, 704, 7), -- Enter City
 	[50142] = Position(1475, 688, 11), -- Depot
 	[50143] = Position(574, 1175, 9), -- Elfic Ville Cave
 	[50144] = Position(1580, 743, 7), -- Forgotten Town
