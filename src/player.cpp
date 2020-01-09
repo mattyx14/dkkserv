1229ec558a2dae258d7628ea2b9b55078df5d3ac
Matt Gomez <mattyx14@users.noreply.github.com>
2020-01-09 06:22:53 -0600

    fix client crash when trading lot of items with npcs

--- src/player.cpp
+++ src/player.cpp
@@ -3116,20 +3116,12 @@ void Player::postAddNotification(Thing* thing, const Cylinder* oldParent, int32_
 		g_moveEvents->onPlayerEquip(this, thing->getItem(), static_cast<slots_t>(index), false);
 	}
 
-	bool requireListUpdate = true;
-
 	if (link == LINK_OWNER || link == LINK_TOPPARENT) {
 		const Item* i = (oldParent ? oldParent->getItem() : nullptr);
 
 		// Check if we owned the old container too, so we don't need to do anything,
 		// as the list was updated in postRemoveNotification
 		assert(i ? i->getContainer() != nullptr : true);
-
-		if (i) {
-			requireListUpdate = i->getContainer()->getHoldingPlayer() != this;
-		} else {
-			requireListUpdate = oldParent != this;
-		}
 
 		updateInventoryWeight();
 		updateItemsLight();
@@ -3140,10 +3132,6 @@ void Player::postAddNotification(Thing* thing, const Cylinder* oldParent, int32_
 	if (const Item* item = thing->getItem()) {
 		if (const Container* container = item->getContainer()) {
 			onSendContainer(container);
-		}
-
-		if (shopOwner && requireListUpdate) {
-			updateSaleShopList();
 		}
 	} else if (const Creature* creature = thing->getCreature()) {
 		if (creature == this) {
@@ -3171,20 +3159,12 @@ void Player::postRemoveNotification(Thing* thing, const Cylinder* newParent, int
 		g_moveEvents->onPlayerDeEquip(this, thing->getItem(), static_cast<slots_t>(index));
 	}
 
-	bool requireListUpdate = true;
-
 	if (link == LINK_OWNER || link == LINK_TOPPARENT) {
 		const Item* i = (newParent ? newParent->getItem() : nullptr);
 
 		// Check if we owned the old container too, so we don't need to do anything,
 		// as the list was updated in postRemoveNotification
 		assert(i ? i->getContainer() != nullptr : true);
-
-		if (i) {
-			requireListUpdate = i->getContainer()->getHoldingPlayer() != this;
-		} else {
-			requireListUpdate = newParent != this;
-		}
 
 		updateInventoryWeight();
 		updateItemsLight();
@@ -3219,17 +3199,6 @@ void Player::postRemoveNotification(Thing* thing, const Cylinder* newParent, int
 				autoCloseContainers(container);
 			}
 		}
-
-		if (shopOwner && requireListUpdate) {
-			updateSaleShopList();
-		}
-	}
-}
-
-void Player::updateSaleShopList()
-{
-	if (client) {
-		client->sendSaleItemList(shopItemList);
 	}
 }
 
