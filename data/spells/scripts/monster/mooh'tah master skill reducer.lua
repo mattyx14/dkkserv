343fc82312f4d517687205f0ccb67b740e0e08b7
Matt Gomez <mattyx14@users.noreply.github.com>
2020-01-09 06:29:40 -0600

    fix skill reducer

--- data/spells/scripts/monster/mooh'tah master skill reducer.lua
+++ data/spells/scripts/monster/mooh'tah master skill reducer.lua
@@ -4,7 +4,7 @@
 
 	local condition = Condition(CONDITION_ATTRIBUTES)
 	condition:setParameter(CONDITION_PARAM_TICKS, 3000)
-	condition:setParameter(CONDITION_PARAM_SKILL_SHIELDPERCENT, -100)
+	condition:setParameter(CONDITION_PARAM_DISABLE_DEFENSE, true)
 	combat:addCondition(condition)
 
 function onCastSpell(creature, var)
--- data/spells/scripts/monster/pixie skill reducer.lua
+++ data/spells/scripts/monster/pixie skill reducer.lua
@@ -4,7 +4,7 @@ combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_PIXIE_EXPLOSION)
 local condition = Condition(CONDITION_ATTRIBUTES)
 condition:setParameter(CONDITION_PARAM_TICKS, 6000)
 condition:setParameter(CONDITION_PARAM_SKILL_DISTANCE, -70)
-condition:setParameter(CONDITION_PARAM_SKILL_SHIELD, -70)
+condition:setParameter(CONDITION_PARAM_DISABLE_DEFENSE, true)
 condition:setParameter(CONDITION_PARAM_SKILL_MELEE, -70)
 
 local area = createCombatArea(AREA_CIRCLE2X2)
