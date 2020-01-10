8db05017171877465ed8138a5da608f5bc68c8dc
Matt Gomez <mattyx14@users.noreply.github.com>
2020-01-10 06:55:31 -0600

    remove foxtail from deepling outfit fix hive outfitquest other fixes on items rewards

--- data/actions/scripts/quests/outfits/outfits.lua
+++ data/actions/scripts/quests/outfits/outfits.lua
@@ -4,13 +4,13 @@ local outfits = {
 	[10008] = {10008, 'Death Herald', 666, 667, 21252}, -- 10005, 10006 and 10007 is reserved storage for kill Dracula
 	[9933] = {9933, 'Assassin', 156, 152, 9933},
 	[8820] = {8820, 'Chaos Acolyte', 664, 665, 8820},
-	[15545] = {15545, 'Deepling', 464, 463, 15545},
+	[15545] = {15545, 'Deepling', 464, 463, 15408},
 	[7431] = {7431, 'Demonhunter', 288, 289, 7431},
 	[5807] = {5807, 'Brotherhood', 279, 278, 5807},
 	[8267] = {8267, 'Oriental', 150, 146, 8267},
 	[3697] = {3697, 'Marry', 329, 328, 2121},
 	[1449] = {1449, 'Puppeteer', 696, 697, 3972},
-	[14699] = {14699, 'Insectoid', 466, 465, 14699},
+	[14699] = {14699, 'Insectoid', 466, 465, 15643},
 }
 
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
