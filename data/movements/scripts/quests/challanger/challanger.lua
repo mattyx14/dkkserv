ec66c03a54f107fa9fa56c8b0553f0c2379e634f
Matt Gomez <mattyx14@users.noreply.github.com>
2020-01-20 16:56:25 -0600

    fix challanger portals

--- data/movements/scripts/quests/challanger/challanger.lua
+++ data/movements/scripts/quests/challanger/challanger.lua
@@ -1,6 +1,13 @@
 local destination = {
-	[24878] = {newPos = Position(1127, 1080, 15), backPos = Position(1095, 1080, 15), timer = Storage.ForgottenKnowledge.LadyTenebrisTimer},
-	[24879] = {newPos = Position(1133, 461, 14), backPos = Position(1138, 472, 14), timer = Storage.MisidiaQuest.jaulTimer}
+	[24878] = { -- Lady Tenebris
+		newPos = Position(1127, 1080, 15),
+		backPos = Position(1095, 1080, 15),
+		timer = Storage.ForgottenKnowledge.LadyTenebrisTimer},
+	[24879] = { -- Jaul
+		newPos = Position(1133, 461, 14),
+		backPos = Position(1138, 472, 14),
+		timer = Storage.MisidiaQuest.jaulTimer
+	},
 }
 
 function onStepIn(creature, item, position, fromPosition)
@@ -10,19 +17,15 @@ function onStepIn(creature, item, position, fromPosition)
 	end
 
 	local teleport = destination[item.uid]
-	if not teleport then
-		return
-	end
-	if player:getExhaustion(teleport.timer) > 0 then
+	if player:getStorageValue(teleport.timer) > 0 then
 		position:sendMagicEffect(CONST_ME_TELEPORT)
-		player:teleportTo(teleport.newPos)
-		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-		return true
-	else
 		player:teleportTo(teleport.backPos)
-		player:say('You have to wait to challange this enemy again!', TALKTYPE_MONSTER_SAY)
 		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+		player:say('You have to wait to challange this enemy again!', TALKTYPE_MONSTER_SAY)
 		return true
 	end
+	position:sendMagicEffect(CONST_ME_TELEPORT)
+	player:teleportTo(teleport.newPos)
+	player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 	return true
 end
