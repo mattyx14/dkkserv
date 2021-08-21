800733eca4e88be968a3d414525a2b6b8acf7438
Matt Gomez <martin_jojo@hotmail.com>
2021-08-20 21:47:42 -0500

    Sohan Town Fixes
    Added missing npc
    Fix House doors on Sohan Town
    // Tomorrow Fynn and Anshara

--- data/startup/tables/npc_spawn.lua
+++ data/startup/tables/npc_spawn.lua
@@ -1,32 +1,49 @@
 NpcTable = {
 	--[] = {name = "", position = Position()},
 	-- Fynn Castle
-	[1] = {name = "Yura", position = Position(912, 999, 7)},
-	[2] = {name = "Edson", position = Position(893, 995, 7)},
-	[3] = {name = "Gloria", position = Position(885, 996, 7)},
-	[4] = {name = "Vancer", position = Position(909, 1019, 7)},
-	[5] = {name = "Wendy", position = Position(930, 1016, 7)},
-	[6] = {name = "Dan", position = Position(969, 1006, 7)},
-	[7] = {name = "Yuna", position = Position(976, 1017, 7)},
-	[8] = {name = "Albinius", position = Position(940, 995, 7)},
-	[9] = {name = "Palacios", position = Position(948, 977, 7)},
-	[10] = {name = "Javier", position = Position(957, 1025, 6)},
-	[11] = {name = "Henrry", position = Position(880, 1005, 8)},
-	[12] = {name = "The King", position = Position(971, 932, 7)},
-	[13] = {name = "Katty", position = Position(980, 965, 8)},
-	[14] = {name = "Citizen_addon1_fynn", position = Position(978, 1049, 7)},
-	[15] = {name = "Citizen_women_addon2_fynn", position = Position(971, 952, 7)},
+	[1] = {name = "Yura", position = Position(912, 999, 7)}, -- Potion / Rune
+	[2] = {name = "Edson", position = Position(893, 995, 7)}, -- Food
+	[3] = {name = "Gloria", position = Position(885, 996, 7)}, -- Furniture
+	[4] = {name = "Vancer", position = Position(909, 1019, 7)}, -- Munition
+	[5] = {name = "Wendy", position = Position(930, 1016, 7)}, -- Weapon
+	[6] = {name = "Dan", position = Position(969, 1006, 7)}, -- Mail
+	[7] = {name = "Yuna", position = Position(976, 1017, 7)}, -- Tools
+	[8] = {name = "Albinius", position = Position(940, 995, 7)}, -- Imbuiment
+	[9] = {name = "Palacios", position = Position(948, 977, 7)}, -- Bank
+	[10] = {name = "Javier", position = Position(957, 1025, 6)}, -- Boat
+	[11] = {name = "Henrry", position = Position(880, 1005, 8)}, -- Gems
+	[12] = {name = "The King", position = Position(971, 932, 7)}, -- Promoter
+	[13] = {name = "Katty", position = Position(980, 965, 8)}, -- Blessings
+	[14] = {name = "Citizen_addon1_fynn", position = Position(978, 1049, 7)}, -- Custom "Citizen Addoner"
+	[15] = {name = "Citizen_women_addon2_fynn", position = Position(971, 952, 7)}, -- Custom "Citizen Addoner"
 	-- Anshara Desert
-	[16] = {name = "Albinius", position = Position(721, 449, 7)},
-	[17] = {name = "Katty", position = Position(700, 448, 5)},
-	[18] = {name = "Palacios", position = Position(636, 439, 4)},
-	[19] = {name = "Henrry", position = Position(728, 453, 5)},
-	[20] = {name = "Yuna", position = Position(714, 453, 4)},
-	[21] = {name = "Vancer", position = Position(728, 453, 4)},
-	[22] = {name = "Yura", position = Position(714, 453, 5)},
-	[23] = {name = "Dan", position = Position(636, 440, 5)},
-	[24] = {name = "Wendy", position = Position(684, 380, 7)},
-	[25] = {name = "Josh", position = Position(645, 360, 6)},
-	[26] = {name = "JokhHah", position = Position(626, 348, 5)},
+	[16] = {name = "Yura", position = Position(714, 453, 5)}, -- Potion / Rune
+	[17] = {name = "Edson", position = Position(715, 439, 5)}, -- Food
+	[18] = {name = "Gloria", position = Position(727, 439, 5)}, -- Furniture
+	[19] = {name = "Vancer", position = Position(728, 453, 4)}, -- Munition
+	[20] = {name = "Wendy", position = Position(684, 380, 7)}, --Weapon
+	[21] = {name = "Dan", position = Position(636, 440, 5)}, -- Mail
+	[22] = {name = "Yuna", position = Position(714, 453, 4)}, -- Tools
+	[23] = {name = "Albinius", position = Position(721, 449, 7)}, -- Imbuiment
+	[24] = {name = "Palacios", position = Position(636, 439, 4)}, -- Bank
+	[25] = {name = "Josh", position = Position(645, 360, 6)}, -- Boat
+	[26] = {name = "Henrry", position = Position(728, 453, 5)}, -- Gems
+	[27] = {name = "Mony", position = Position(721, 447, 5)}, -- Promoter
+	[28] = {name = "Katty", position = Position(700, 448, 5)}, -- Blessings
+	[29] = {name = "JokhHah", position = Position(626, 348, 5)}, -- Djinn
 	-- Sohan Town
+	[30] = {name = "Yura", position = Position(365, 1117, 7)}, -- Potion / Rune
+	[31] = {name = "Edson", position = Position(359, 1097, 7)}, -- Food
+	[32] = {name = "Gloria", position = Position(377, 1117, 6)}, -- Furniture
+	[33] = {name = "Vancer", position = Position(390, 1141, 7)}, -- Munition
+	[34] = {name = "Wendy", position = Position(348, 1120, 7)}, --Weapon
+	[35] = {name = "Dan", position = Position(320, 1083, 7)}, -- Mail
+	[36] = {name = "Yuna", position = Position(348, 1114, 7)}, -- Tools
+	[37] = {name = "Albinius", position = Position(337, 1091, 6)}, -- Imbuiment
+	[38] = {name = "Palacios", position = Position(389, 1087, 7)}, -- Bank
+	[39] = {name = "Ramon", position = Position(340, 1127, 5)}, -- Boat
+	[40] = {name = "Henrry", position = Position(423, 1104, 7)}, -- Gems
+	[41] = {name = "Sammuel", position = Position(341, 1091, 5)}, -- Promoter
+	[42] = {name = "Katty", position = Position(334, 1091, 4)}, -- Blessings
+	--[] = {name = "Guardian", position = Position()}, -- Custom "Guardian"
 }
