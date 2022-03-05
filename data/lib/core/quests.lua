47bfe1e35e07a018730a0dbf20cedb5aca79b8f0
Matt Gomez <mattyx14@users.noreply.github.com>
2022-03-05 09:48:59 -0600

    test

--- data/lib/core/quests.lua
+++ data/lib/core/quests.lua
@@ -1,42 +1,20 @@
 if not Quests then
 	Quests = {
 		[1] = {
-			name = "Forgotten Knowledge",
-			startStorageId = DarkKonia.ForgottenKnowledge.Tomes,
+			name = "Example",
+			startStorageId = Storage.Quest.ExampleQuest.Example,
 			startStorageValue = 1,
 			missions = {
 				[1] = {
-					name = "The temple of glory",
-					storageId = DarkKonia.ForgottenKnowledge.Tomes,
-					missionId = 10360,
-					startValue = 0,
+					name = "The Hidden Seal",
+					storageId = Storage.Quest.ExampleQuest.Example,
+					missionId = 1,
+					startValue = 1,
 					endValue = 1,
-					states = {
-						[1] = "You have contributed with the collection of tomes."
-					}
-				},
-				[2] = {
-					name = "Circle of the Black Sphinx",
-					storageId = DarkKonia.ForgottenKnowledge.LadyTenebrisKilled,
-					missionId = 10361,
-					startValue = 0,
-					endValue = 1522018605,
-					states = {
-						[1] = "You defeated the rogue Lady Tenebris."
-					}
-				},
-				[3] = {
-					name = "Bane of the deep",
-					storageId = DarkKonia.MisidiaQuest.JaulKilled,
-					missionId = 10362,
-					startValue = 0,
-					endValue = 1522018605,
-					states = {
-						[1] = "You defeated the terrifying Jaul."
-					}
-				},
+					description = "You broke the first seal."
+				}
 			}
-		},
+		}
 	}
 end
 
