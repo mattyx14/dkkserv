f0d88134e55364f19d28215bd2d1a5bec93ed802
mattyx14 <martin_jojo@hotmail.com>
2025-03-24 16:10:17 -0600

    formated

--- data-otxserver/lib/core/load.lua
+++
@@ -1,4 +0,0 @@
-dofile(DATA_DIRECTORY .. "/lib/core/storages.lua")
-dofile(DATA_DIRECTORY .. "/lib/core/constants.lua")
-dofile(DATA_DIRECTORY .. "/lib/core/storagesCustom.lua")
-dofile(DATA_DIRECTORY .. "/lib/core/quests.lua")
--- data-otxserver/scripts/actions/tools/rake.lua
+++
@@ -1,8 +0,0 @@
-local rake = Action()
-
-function rake.onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	return true
-end
-
-rake:id(3452)
-rake:register()
